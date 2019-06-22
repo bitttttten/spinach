bb7c77bf7da38048fef9fec325494a94cc389000
bitttttten <bitttttten@googlemail.com>
2019-06-22 22:59:02 +0200

    first draft of turning file into buffer and passing it through image imagemin

--- src/index.js
+++ src/index.js
@@ -1,54 +1,78 @@
 import path from 'path';
 
+import imagemin = from 'imagemin';
+import imageminJpegtran = from 'imagemin-jpegtran';
+import imageminPngquant = from 'imagemin-pngquant';
 import loaderUtils from 'loader-utils';
 import validateOptions from 'schema-utils';
 
-import schema from './options.json';
+import schema from './options.json'
+
+const jpegPlugin = imageminJpegtran(),
+const pngPlugin = imageminPngquant({
+	quality: [0.6, 0.8]
+})
 
 export default function loader(content) {
-  const options = loaderUtils.getOptions(this) || {};
+    const callback = this.async();
+	this.cacheable && this.cacheable();
+	this.value = content;
 
-  validateOptions(schema, options, 'Spinach');
+    const options = loaderUtils.getOptions(this) || {};
 
-  const context = options.context || this.rootContext;
+    validateOptions(schema, options, 'Spinach');
 
-  const url = loaderUtils.interpolateName(this, options.name, {
-    context,
-    content,
-  });
+	const context = options.context || this.rootContext;
+	
+    async function run() {
+        const url = loaderUtils.interpolateName(this, options.name, {
+            context,
+            content,
+        });
 
-  let outputPath = url;
+        let outputPath = url;
 
-  if (options.outputPath) {
-    if (typeof options.outputPath === 'function') {
-      outputPath = options.outputPath(url, this.resourcePath, context);
-    } else {
-      outputPath = path.posix.join(options.outputPath, url);
+        if (options.outputPath) {
+            if (typeof options.outputPath === 'function') {
+                outputPath = options.outputPath(url, this.resourcePath, context);
+            } else {
+                outputPath = path.posix.join(options.outputPath, url);
+            }
+        }
+
+        let publicPath = `__webpack_public_path__ + ${JSON.stringify(outputPath)}`;
+
+        if (options.publicPath) {
+            if (typeof options.publicPath === 'function') {
+                publicPath = options.publicPath(url, this.resourcePath, context);
+            } else {
+                publicPath = `${
+				options.publicPath.endsWith('/')
+					? options.publicPath
+					: `${options.publicPath}/`
+				}${url}`;
+            }
+
+            publicPath = JSON.stringify(publicPath);
+		}
+
+		const buffer = Buffer.from(`"${content.toString('base64')}"`, "base64")
+
+		const files = await imagemin.buffer(buffer, {
+			plugins: [
+				jpegPlugin,
+				pngPlugin,
+			]
+		});
+	
+		console.log(files);
+
+		return `module.exports = ${publicPath};`
     }
-  }
 
-  let publicPath = `__webpack_public_path__ + ${JSON.stringify(outputPath)}`;
-
-  if (options.publicPath) {
-    if (typeof options.publicPath === 'function') {
-      publicPath = options.publicPath(url, this.resourcePath, context);
-    } else {
-      publicPath = `${
-        options.publicPath.endsWith('/')
-          ? options.publicPath
-          : `${options.publicPath}/`
-      }${url}`;
-    }
-
-    publicPath = JSON.stringify(publicPath);
-  }
-
-  if (typeof options.emitFile === 'undefined' || options.emitFile) {
-    this.emitFile(outputPath, content);
-  }
-
-  // TODO revert to ES2015 Module export, when new CSS Pipeline is in place
-  return `module.exports = ${publicPath};`;
+    run().then((result) => {
+		callback(null, result)
+    })
 }
 
 export const raw = true;
